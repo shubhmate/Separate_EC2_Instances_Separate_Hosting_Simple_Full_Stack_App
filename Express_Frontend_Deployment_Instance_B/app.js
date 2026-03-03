01c3c8cc0e3d2a1f7ce7514936b6c873d8632975
Ubuntu <ubuntu@ip-172-31-2-23.ec2.internal>
2026-03-03 16:07:46 +0000

    Frontend_Nginx_Config 'sudo vi /etc/nginx/sites-available/express_app'

--- Express_Frontend_Deployment_Instance_B/app.js
+++ Express_Frontend_Deployment_Instance_B/app.js
@@ -6,7 +6,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.post('/submit', async (req, res) => {
-    const response = await axios.post('http://44.204.143.61:80/process', req.body);
+    const response = await axios.post('http://<INSTANCE_A_IP>:80/process', req.body);
     res.send(response.data.message);
 });
 app.listen(3000, () => console.log('Frontend on 3000'));
